88beb02a1b06f7e76c077ef33f13ca0fd76dbde7
NiekRottier <niekrottier@gmail.com>
2021-05-11 20:10:25 +0200

    Changed to react-three-fiber

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,42 +1,33 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 import * as THREE from 'three';
-import { useEffect } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useState ,useRef } from 'react';
 
 const Tab3: React.FC = () => {
-
-  useEffect(() => {
-    const rendererContainer = document.getElementById("rendererContainer");
-
-    // Create scene and camera
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, rendererContainer.offsetWidth / rendererContainer.offsetHeight, 0.1, 1000 );
-    camera.position.z = 5;
-
-    // Create renderer (view in which the scene is rendered)
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( rendererContainer.offsetWidth, rendererContainer.offsetHeight );
-    rendererContainer.appendChild(renderer.domElement)
-
-    // Create cube
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00  } );
-    const cube = new THREE.Mesh( geometry, material )
-    // Add cube to scene
-    scene.add( cube );
-    
-    // Render
-    renderer.render( scene, camera );
-
-    // Animate the cube
-    function animate() {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    }
-    animate();
-  });
+  
+  function Box(props: JSX.IntrinsicElements['mesh']) {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef<THREE.Mesh>(null!)
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Rotate mesh every frame, this is outside of React without overhead
+    useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  
+    return (
+      <mesh
+        {...props}
+        ref={mesh}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+  }
   
   return (
     <IonPage>
@@ -45,7 +36,13 @@ const Tab3: React.FC = () => {
           <IonTitle>AR resultaat</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen id="rendererContainer">
+      <IonContent fullscreen>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
       </IonContent>
     </IonPage>
   );
