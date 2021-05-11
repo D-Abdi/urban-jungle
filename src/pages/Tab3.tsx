52f1a7d4e44f6e87906ad5cd4d96bb9b99a4e3c4
NiekRottier <niekrottier@gmail.com>
2021-05-11 14:56:33 +0200

    Added cube using three.js

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,51 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import * as THREE from 'three';
+import { useEffect } from 'react';
 
 const Tab3: React.FC = () => {
+
+  useEffect(() => {
+    const rendererContainer = document.getElementById("rendererContainer");
+
+    // Create scene and camera
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, rendererContainer.offsetWidth / rendererContainer.offsetHeight, 0.1, 1000 );
+    camera.position.z = 5;
+
+    // Create renderer (view in which the scene is rendered)
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( rendererContainer.offsetWidth, rendererContainer.offsetHeight );
+    rendererContainer.appendChild(renderer.domElement)
+
+    // Create cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00  } );
+    const cube = new THREE.Mesh( geometry, material )
+    // Add cube to scene
+    scene.add( cube );
+    
+    // Render
+    renderer.render( scene, camera );
+
+    // Animate the cube
+    function animate() {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    }
+    animate();
+  });
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>AR resultaat</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      <IonContent fullscreen id="rendererContainer">
       </IonContent>
     </IonPage>
   );
