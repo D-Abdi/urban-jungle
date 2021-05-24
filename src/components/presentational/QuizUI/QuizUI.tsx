3e5c6ec333eb5bc19d0b7f4b23b08bd853688557
MJ1109 <manuja115@gmail.com>
2021-05-24 17:13:05 +0200

    halfway there again ;')

--- src/components/presentational/QuizUI/QuizUI.tsx
+++ src/components/presentational/QuizUI/QuizUI.tsx
@@ -2,37 +2,61 @@ import { IonCardSubtitle } from '@ionic/react';
 import * as React from 'react';
 import { useState } from 'react';
 
-type Props = {
-    questionNr: number;
-    totalQuestions: number;
-    question: string;
-    answers: [];
-}
+import axios from 'axios';
 
-const QuizUI: React.FC<Props> = ({questionNr, totalQuestions, question, answers}) => {
-    const [score, setScore] = useState(0)
+const QuizUI: React.FC = () => {
+    const [score, setScore] = useState(0);
+    const [items, setItems] = useState(null);
+    const [questionNr, setQuestionNr] = useState(1);
+    const [totalQuestions, setTotalQuestions] = useState(9);
+    const [question, setQuestion] = useState(null);
+    const [answers, setAnswers] = useState(null);
+    
     function chooseAnswer(answer) {
         console.log("Choose answer: " + answer[0]);
         setScore(score + answer[1])
         console.log(score);
     }
 
-    return(
-        <div className="question">
-            <IonCardSubtitle>
-                Question: {questionNr} / {totalQuestions}
-            </IonCardSubtitle>
-            <p>{question}</p>
-            <div>
-                {answers.map((answer, index) => (
-                    <div key={index} className="answer" onClick={(e) => chooseAnswer(answer)}>
-                        {answer[0]}
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+    function setQA (number){
+        if(items !== null){
+        setQuestion(items[number].question)
+        setAnswers(items[number].answers)
+        }
+    }
+    
+    React.useEffect(()=>{
+        setQA(0)
+    })
 
+    React.useEffect(() => {
+        axios.get("/assets/questions/Questions.json")
+            .then(
+                res => {
+                    setItems(res.data); 
+                }
+            )
+            .then(()=>setQA(0))
+    }, [])
+    if(answers && answers !== null){
+        return(
+            <div className="question">
+                <IonCardSubtitle>
+                    Question: {questionNr} / {totalQuestions}
+                </IonCardSubtitle>
+                <p>{question}</p>
+                <div>
+                    {answers.map((answer, index) => (
+                        <div key={index} className="answer" onClick={(e) => chooseAnswer(answer)}>
+                            {answer[0]}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }else {
+        return <p>Aan het laden... </p>
+    }
 }
 
 export default QuizUI;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -5,39 +5,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import QuizUI from '../components/presentational/QuizUI/QuizUI';
 import './Tab2.css';
 
-import axios from 'axios';
-
-function Quiz(){
-  const [items, setItems] = useState(null);
-  React.useEffect(() => {
-    axios.get("/assets/questions/Questions.json")
-      .then(
-        res => {
-          setItems(res.data);      
-        },
-        (error) => {
-          console.log(error);
-        }
-      )
-  }, [])
-
-  if(items && items !== null){
-    return (
-      <QuizUI 
-        questionNr={1}
-        totalQuestions={2}
-        question={items[0].question}
-        answers={items[0].answers}
-      />
-    )   
-  }else {
-    return(
-      <p> Aan het laden!</p>
-    )
-  }
-}
 const Tab2: React.FC = () => {
-
 
   return (
     <IonPage>
@@ -47,7 +15,7 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <Quiz/>
+        <QuizUI/>
       </IonContent>
     </IonPage>
   );
