8bcf0b15ca711cc283985f5a1cae5c5494465144
MJ1109 <manuja115@gmail.com>
2021-05-24 15:05:05 +0200

    inbetween final and dev

---
+++ src/components/presentational/QuizUI/QuizUI.tsx
@@ -0,0 +1,35 @@
+import { IonCardSubtitle } from '@ionic/react';
+import * as React from 'react';
+import { useState } from 'react';
+
+type Props = {
+    questionNr: number;
+    totalQuestions: number;
+    question: string;
+    answers: string[];
+}
+
+const QuizUI: React.FC<Props> = ({questionNr, totalQuestions, question, answers}) => {
+    function chooseAnswer(e) {
+        console.log("Choose answer: " + e);
+    }
+
+    return(
+        <div className="question">
+            <IonCardSubtitle>
+                Question: {questionNr} / {totalQuestions}
+            </IonCardSubtitle>
+            <p>{question}</p>
+            <div>
+                {answers.map((answer, index) => (
+                    <div key={index} className="answer" onClick={(e) => chooseAnswer(e)}>
+                        {answer}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+
+}
+
+export default QuizUI;
